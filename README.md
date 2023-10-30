5233b60551a96b98b1a7784723dba263f4875411
Alefe Souza <contact@alefesouza.com>
2023-10-29 22:22:18 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# viajarcomale.com
+
+This repository contains the code for the viajarcomale.com (Viajar com Alê) website, the template is inspired on link in bio platforms using Google Analytics and PWA concepts like full `manifest.json`, service worker and offline access.
---
+++ serviceworker.js
@@ -0,0 +1,62 @@
+const cacheName = 'v1';
+
+const addResourcesToCache = async (resources) => {
+  const cache = await caches.open(cacheName);
+  await cache.addAll(resources);
+};
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    addResourcesToCache([
+      '/',
+      '/index.html',
+      '/favicon.ico',
+      '/app.js',
+      '/manifest.json',
+      '/profile-photo.jpg',
+      '/icons/36x36.png',
+      '/icons/48x48.png',
+      '/icons/60x60.png',
+      '/icons/72x72.png',
+      '/icons/76x76.png',
+      '/icons/96x96.png',
+      '/icons/120x120.png',
+      '/icons/144x144.png',
+      '/icons/152x152.png',
+      '/icons/167x167.png',
+      '/icons/180x180.png',
+      '/icons/192x192.png',
+      '/icons/512x512.png',
+      '/images/360.jpeg',
+      '/images/asexplore.png',
+      '/images/twitch.png',
+      '/logos/instagram.png',
+      '/logos/instagram96.png',
+      '/logos/tiktok.png',
+      '/logos/tiktok96.png',
+      '/logos/youtube.png',
+      '/logos/youtube96.png',
+      '/screenshots/1.png',
+      '/screenshots/2.png',
+      '/screenshots/3.png',
+      '/screenshots/4.png'
+    ]),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    (async () => {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })(),
+  );
+});
