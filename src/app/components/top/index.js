14e7e7546f88da79544b1222786a8685bfeca3ef
Alefe Souza <contact@alefesouza.com>
2023-11-06 04:09:33 -0300

    Fix styling

--- src/app/components/top/index.js
+++ src/app/components/top/index.js
@@ -1,11 +1,14 @@
 import useHost from '@/app/hooks/use-host';
+import Link from '@/app/link/page';
 import countries from '@/app/utils/countries';
 
 export default function Top() {
   const host = useHost();
   
   return <div className="profile">
-    <img src={ host('profile-photo.jpg') } srcSet={ host('profile-photo-2x.jpg') + ' 2x' } width={96} height={96} alt="Profile photo" className="profile_picture" />
+    <Link href={host('/')}>
+      <img src={ host('profile-photo.jpg') } srcSet={ host('profile-photo-2x.jpg') + ' 2x' } width={96} height={96} alt="Profile photo" className="profile_picture" />
+    </Link>
 
     <span className="profile_name">Viajar com Alê</span>
 
