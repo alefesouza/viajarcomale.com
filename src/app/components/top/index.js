509f9e2079bfd297bac664d6e8857072848b61cb
Alefe Souza <contact@alefesouza.com>
2023-11-06 04:14:12 -0300

    Fix styling

--- src/app/components/top/index.js
+++ src/app/components/top/index.js
@@ -1,12 +1,12 @@
 import useHost from '@/app/hooks/use-host';
-import Link from '@/app/link/page';
 import countries from '@/app/utils/countries';
+import Link from 'next/link';
 
 export default function Top() {
   const host = useHost();
   
   return <div className="profile">
-    <Link href={host('/')}>
+    <Link href={ host('/') }>
       <img src={ host('profile-photo.jpg') } srcSet={ host('profile-photo-2x.jpg') + ' 2x' } width={96} height={96} alt="Profile photo" className="profile_picture" />
     </Link>
 
