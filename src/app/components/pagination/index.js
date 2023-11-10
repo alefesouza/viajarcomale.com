6683f788070894d73f95a44e4460c33548caee16
Alefe Souza <contact@alefesouza.com>
2023-11-10 02:36:32 -0300

    Remove prefetch from pagination

--- src/app/components/pagination/index.js
+++ src/app/components/pagination/index.js
@@ -39,20 +39,20 @@ export default function Pagination({ base, currentPage, pageNumber, total, textP
     {textPosition === 'top' && totalText}
 
     <div className={styles.mobile_pagination}>
-      <Link href={currentPage == 2 ? base.replace('/page/{page}', '') : base.replace('{page}', currentPage - 1)} scroll={false} className={ currentPage == 1 ? styles.pagination_disabled : null}>&lt;</Link>
+      <Link href={currentPage == 2 ? base.replace('/page/{page}', '') : base.replace('{page}', currentPage - 1)} scroll={false} className={ currentPage == 1 ? styles.pagination_disabled : null} prefetch={false}>&lt;</Link>
       {currentPage > 2 && pageNumber > 5 && <>
         <Link href={base.replace('/page/{page}', '')} className={styles.page_number} scroll={false}>1</Link>
-        <a href="#" className={ styles.skip }>…</a>
+        <a href="#" className={ styles.skip } prefetch={false}>…</a>
       </>}
-      {mobilePages.length > 1 && mobilePages.map((p) => <Link key={p} href={p == 1 ? base.replace('/page/{page}', '') : base.replace('{page}', p)} className={styles.page_number + (p == currentPage ? ' ' + styles.page_number_active : '')} scroll={false}>{p}</Link>)}
+      {mobilePages.length > 1 && mobilePages.map((p) => <Link key={p} href={p == 1 ? base.replace('/page/{page}', '') : base.replace('{page}', p)} className={styles.page_number + (p == currentPage ? ' ' + styles.page_number_active : '')} scroll={false} prefetch={false}>{p}</Link>)}
       {currentPage < pageNumber - 2 && pageNumber > 5 && <a href="#" className={ styles.skip }>…</a>}
-      {currentPage < pageNumber - 1 && pageNumber > 5 && <Link href={base.replace('{page}', pageNumber)} className={styles.page_number} scroll={false}>{pageNumber}</Link>}
-      <Link href={base.replace('{page}', currentPage + 1)} scroll={false} className={ currentPage == pageNumber ? styles.pagination_disabled : null}>&gt;</Link>
+      {currentPage < pageNumber - 1 && pageNumber > 5 && <Link href={base.replace('{page}', pageNumber)} className={styles.page_number} scroll={false} prefetch={false}>{pageNumber}</Link>}
+      <Link href={base.replace('{page}', currentPage + 1)} scroll={false} className={ currentPage == pageNumber ? styles.pagination_disabled : null} prefetch={false}>&gt;</Link>
     </div>
     <div className={styles.desktop_pagination}>
-      <Link href={currentPage == 2 ? base.replace('/page/{page}', '') : base.replace('{page}', currentPage - 1)} scroll={false} className={ currentPage == 1 ? styles.pagination_disabled : null}>{i18n('Previous')}</Link>
-      {pages.map((p) => <Link key={p} href={p == 1 ? base.replace('/page/{page}', '') : base.replace('{page}', p)} className={styles.page_number + (p == currentPage ? ' ' + styles.page_number_active : '')} scroll={false}>{p}</Link>)}
-      <Link href={base.replace('{page}', currentPage + 1)} scroll={false} className={ currentPage == pageNumber ? styles.pagination_disabled : null}>{i18n('Next')}</Link>
+      <Link href={currentPage == 2 ? base.replace('/page/{page}', '') : base.replace('{page}', currentPage - 1)} scroll={false} className={ currentPage == 1 ? styles.pagination_disabled : null} prefetch={false}>{i18n('Previous')}</Link>
+      {pages.map((p) => <Link key={p} href={p == 1 ? base.replace('/page/{page}', '') : base.replace('{page}', p)} className={styles.page_number + (p == currentPage ? ' ' + styles.page_number_active : '')} scroll={false} prefetch={false}>{p}</Link>)}
+      <Link href={base.replace('{page}', currentPage + 1)} scroll={false} className={ currentPage == pageNumber ? styles.pagination_disabled : null} prefetch={false}>{i18n('Next')}</Link>
     </div>
 
     {textPosition === 'bottom' && totalText}
