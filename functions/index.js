e07fc7afb7ff96cdb9ed9afd50decde5e6a235f0
Alefe Souza <contact@alefesouza.com>
2023-11-13 02:12:33 -0300

    Update Cloud Functions

--- functions/index.js
+++ functions/index.js
@@ -5,9 +5,6 @@ exports.onMediaCreated = onDocumentCreated('/countries/{countryId}/medias/{media
   const newValue = event.data.data();
   const update = {};
 
-  const countryRef = await event.data.ref.parent.parent.get();
-  const countryData = countryRef.data();
-
   // Set hashtags to an array
   if (typeof newValue.hashtags === 'string') {
     update.hashtags = newValue.hashtags.split('#').map(h => h.trim()).filter(h => h);
@@ -17,31 +14,6 @@ exports.onMediaCreated = onDocumentCreated('/countries/{countryId}/medias/{media
   update.country = split[1];
 
   update.createdAt = admin.firestore.FieldValue.serverTimestamp();
-
-  if (newValue.type === 'instagram') {
-    update.country_index = countryData?.totals?.instagram_photos || 0;
-    const cityIndex = countryData.cities.findIndex(c => c.slug === newValue.city);
-
-    if (countryData.cities[cityIndex]) {
-      update.city_index = countryData.cities[cityIndex]?.totals?.instagram_photos || 0;
-
-      countryData.cities[cityIndex] = {
-        ...countryData.cities[cityIndex],
-        totals: {
-          ...countryData.cities[cityIndex]?.totals,
-          instagram_photos: update.city_index + 1,
-        }
-      }
-    }
-
-    await event.data.ref.parent.parent.update({
-      totals: {
-        ...countryData.totals,
-        instagram_photos: update.country_index + 1,
-      },
-      cities: countryData.cities,
-    })
-  }
 
   return event.data.ref.update(update);
 });
