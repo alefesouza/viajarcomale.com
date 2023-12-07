bc95fbd882c2af442d28293d09bf5aaa0037e3ab
Alefe Souza <contact@alefesouza.com>
2023-12-07 03:00:20 -0300

    Fix AMP errors

--- src/app/webstories/[...page]/route.js
+++ src/app/webstories/[...page]/route.js
@@ -12,9 +12,12 @@ export async function GET(req) {
 
   $('link[href^="/_next"]').remove();
   $('script:not(.amp-asset)').remove();
-  $('style:not(.amp-asset)').remove();
   $('next-route-announcer').remove();
   $('nextjs-portal').remove();
+  $('html').attr('amp', '');
+  $('[standalone]').attr('standalone', '');
+  $('head').append(`<style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style>`)
+  $('head').append(`<noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>`)
 
   return new Response($.html(), {
     headers: { 'Content-Type': 'text/html; charset=utf-8' },
