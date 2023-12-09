17321353516f0a20d253ae63a69fbf8228fd0804
Alefe Souza <contact@alefesouza.com>
2023-12-09 01:34:31 -0300

    Add Firebase Function to update locations

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,8 @@
 const { onDocumentCreated, onDocumentUpdated } = require('firebase-functions/v2/firestore');
+const { getFirestore } = require('firebase-admin/firestore');
 const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 exports.onMediaCreated = onDocumentCreated('/countries/{countryId}/medias/{mediaId}', async (event) => {
   const newValue = event.data.data();
@@ -28,4 +31,25 @@ exports.onMediaUpdated = onDocumentUpdated('/countries/{countryId}/medias/{media
   }
 
   return event.data.after.ref.update(update);
+});
+
+exports.onLocationUpdated = onDocumentUpdated('/countries/{countryId}/locations/{locationId}', async (event) => {
+  const newValue = event.data.after.data();
+
+  const db = getFirestore();
+  const batch = db.batch();
+  const mediasSnapshot = await db.collection('countries').doc(newValue.country).collection('medias').where('locations', 'array-contains', newValue.slug).get();
+
+  mediasSnapshot.forEach((doc) => {
+    const locationData = doc.data().location_data;
+    const locationIndex = locationData.findIndex(l => l.slug === newValue.slug);
+
+    locationData[locationIndex] = newValue;
+
+    batch.update(doc.ref, {
+      location_data: locationData,
+    })
+  });
+
+  return batch.commit();
 });
