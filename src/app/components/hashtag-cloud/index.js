bb0cb561fd1e7a6a633def03569cf131f959bdc3
Alefe Souza <contact@alefesouza.com>
2023-12-23 10:06:34 -0300

    Use bold fonts on Android hashtag cloud

--- src/app/components/hashtag-cloud/index.js
+++ src/app/components/hashtag-cloud/index.js
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 
 import ReactWordcloud from '@/app/components/lib/react-wordcloud';
 
@@ -7,7 +7,12 @@ import 'tippy.js/animations/scale.css';
 import { useState } from 'react';
 import shuffle from '@/app/utils/array-shuffle';
 
-export default function HashtagCloud({ theHashtags, isRandom, shuffleText }) {
+export default function HashtagCloud({
+  theHashtags,
+  isRandom,
+  shuffleText,
+  isAndroid,
+}) {
   const [isLoading, setIsLoading] = useState(false);
   const [hashtags, setHashtags] = useState(theHashtags);
 
@@ -15,15 +20,16 @@ export default function HashtagCloud({ theHashtags, isRandom, shuffleText }) {
     onWordClick: () => {
       document.querySelector('#loader-spinner').style.display = 'block';
     },
-    getWordTooltip: word => `${word.text}`,
-  }
+    getWordTooltip: (word) => `${word.text}`,
+  };
 
   const handleShuffle = async () => {
     const today = new Date().toISOString().split('T')[0];
-    
+
     if (today === localStorage.getItem('hashtags_updated')) {
-      const hashtags = JSON.parse(localStorage.getItem('hashtags'))
-        .map(h => ({ text: '#' + h, value: 5 }));
+      const hashtags = JSON.parse(localStorage.getItem('hashtags')).map(
+        (h) => ({ text: '#' + h, value: 5 })
+      );
       setHashtags(shuffle(hashtags).slice(0, 100));
       return;
     }
@@ -33,38 +39,57 @@ export default function HashtagCloud({ theHashtags, isRandom, shuffleText }) {
     const result = await fetch('/api/hashtags');
     const data = await result.text();
 
-    const hashtags = JSON.parse(data).map(h => ({ text: '#' + h, value: 5 }));
+    setIsLoading(false);
+
+    const hashtags = JSON.parse(data).map((h) => ({ text: '#' + h, value: 5 }));
     setHashtags(shuffle(hashtags).slice(0, 100));
 
     localStorage.setItem('hashtags', data);
-    localStorage.setItem('hashtags_updated', new Date().toISOString().split('T')[0]);
-
-    setIsLoading(false);
-  }
+    localStorage.setItem(
+      'hashtags_updated',
+      new Date().toISOString().split('T')[0]
+    );
+  };
 
   const options = {
     fontFamily: 'Impact',
     fontSizes: [14, 40],
     scale: 'linear',
     spiral: 'archimedean',
+    fontWeight: isAndroid ? 'bold' : 'normal',
     rotations: 0,
     rotationAngles: [-180, 180],
     transitionDuration: 0,
   };
-  
-  return <div>
-    {isRandom && <div className="center_link" style={{ marginBottom: 16 }}>
-      <button onClick={handleShuffle} disabled={isLoading}>{shuffleText}</button>
-    </div>}
 
+  return (
     <div>
-      <ReactWordcloud
-        className="hashtag-cloud"
-        words={typeof window !== 'undefined' && window.innerWidth < 680 && isRandom ? hashtags.slice(0, 50) : hashtags}
-        options={options}
-        callbacks={callbacks}
-        size={[typeof window !== 'undefined' && window.innerWidth < 680 ? window.innerWidth - 24 : 680, isRandom ? 700 : 420]}
-      />
+      {isRandom && (
+        <div className="center_link" style={{ marginBottom: 16 }}>
+          <button onClick={handleShuffle} disabled={isLoading}>
+            {shuffleText}
+          </button>
+        </div>
+      )}
+
+      <div>
+        <ReactWordcloud
+          className="hashtag-cloud"
+          words={
+            typeof window !== 'undefined' && window.innerWidth < 680 && isRandom
+              ? hashtags.slice(0, 50)
+              : hashtags
+          }
+          options={options}
+          callbacks={callbacks}
+          size={[
+            typeof window !== 'undefined' && window.innerWidth < 680
+              ? window.innerWidth - 24
+              : 680,
+            isRandom ? 700 : 420,
+          ]}
+        />
+      </div>
     </div>
-  </div>
+  );
 }
--- src/app/hashtags/page.js
+++ src/app/hashtags/page.js
@@ -8,14 +8,18 @@ import ShareButton from '../components/share-button';
 import HashtagCloud from '../components/hashtag-cloud';
 import shuffle from '../utils/array-shuffle';
 import defaultMetadata from '../utils/default-metadata';
+import { headers } from 'next/headers';
+import { UAParser } from 'ua-parser-js';
 
 export async function generateMetadata() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const i18n = useI18n();
 
-  const title = 'Hashtags' + ' - ' + SITE_NAME;
-  const description = i18n('Navigate through my website with main and random hashtags.');
-  
+  const title = 'Hashtags - ' + SITE_NAME;
+  const description = i18n(
+    'Navigate through my website with main and random hashtags.'
+  );
+
   return defaultMetadata(title, description);
 }
 
@@ -23,7 +27,9 @@ export default async function MapPage() {
   const i18n = useI18n();
   const host = useHost();
   const isBR = host().includes('viajarcomale.com.br');
-  
+  const isAndroid =
+    new UAParser(headers().get('user-agent')).getOS().name === 'Android';
+
   const cacheRef = '/caches/static_pages/static_pages/hashtags_page';
 
   const db = getFirestore();
@@ -32,8 +38,10 @@ export default async function MapPage() {
   let hashtags = [];
 
   if (!cache.exists) {
-    const hashtagsSnapshot = await db.collection('hashtags')
+    const hashtagsSnapshot = await db
+      .collection('hashtags')
       .where('is_place', '==', false)
+      .where('hide_on_cloud', '==', false)
       .limit(100)
       .orderBy('total', 'desc')
       .get();
@@ -46,44 +54,74 @@ export default async function MapPage() {
 
     db.doc(cacheRef).set({
       hashtags,
-      last_update: (new Date().toISOString()).split('T')[0],
+      last_update: new Date().toISOString().split('T')[0],
     });
   } else {
     hashtags = cache.data().hashtags;
   }
 
-  const allHashtagsRef = await db.collection('caches').doc('static_pages').collection('static_pages').doc('hashtags').get();
+  const allHashtagsRef = await db
+    .collection('caches')
+    .doc('static_pages')
+    .collection('static_pages')
+    .doc('hashtags')
+    .get();
   const allHashtagsData = allHashtagsRef.data();
-  const allHashtags = isBR ? allHashtagsData.hashtags_pt : allHashtagsData.hashtags;
+  const allHashtags = isBR
+    ? allHashtagsData.hashtags_pt
+    : allHashtagsData.hashtags;
 
   logAccess(db, host('/hashtags'));
 
-  return <div>
-    <div className="container">
-      <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Link href="/" id="back-button" scroll={false}>
-          <img src={host('/images/back.svg')} alt={i18n('Back')} width="32px"></img>
-        </Link>
+  return (
+    <div>
+      <div className="container">
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Link href="/" id="back-button" scroll={false}>
+            <img
+              src={host('/images/back.svg')}
+              alt={i18n('Back')}
+              width="32px"
+            ></img>
+          </Link>
 
-        <ShareButton />
+          <ShareButton />
+        </div>
+      </div>
+
+      <div className="container">
+        <div>
+          <h2 style={{ marginBottom: 0 }}>Hashtags</h2>
+          <div>{i18n('Click on a hashtag to see my content about it.')}</div>
+        </div>
+
+        <div>
+          <h3>{i18n('Main Hashtags')}</h3>
+          <HashtagCloud
+            theHashtags={hashtags
+              .filter((h) => !h.is_place)
+              .map((h) => ({
+                text: '#' + (isBR && h.name_pt ? h.name_pt : h.name),
+                value: h.total,
+              }))}
+            isBR={isBR}
+            isAndroid={isAndroid}
+          />
+        </div>
+
+        <div>
+          <h3>{i18n('Random Hashtags')}</h3>
+          <HashtagCloud
+            theHashtags={shuffle(allHashtags)
+              .slice(0, 100)
+              .map((c) => ({ text: '#' + c, value: 5 }))}
+            isBR={isBR}
+            shuffleText={i18n('Shuffle')}
+            isRandom
+            isAndroid={isAndroid}
+          />
+        </div>
       </div>
     </div>
-
-    <div className="container">
-      <div>
-        <h2 style={{ marginBottom: 0 }}>Hashtags</h2>
-        <div>{i18n('Click on a hashtag to see my content about it.')}</div>
-      </div>
-
-      <div>
-        <h3>{i18n('Main Hashtags')}</h3>
-        <HashtagCloud theHashtags={hashtags.filter(h => !h.is_place).map((h) => ({ text: '#' + (isBR && h.name_pt ? h.name_pt : h.name), value: h.total }))} isBR={isBR} />
-      </div>
-
-      <div>
-        <h3>{i18n('Random Hashtags')}</h3>
-        <HashtagCloud theHashtags={shuffle(allHashtags).slice(0, 100).map(c => ({ text: '#' + c, value: 5 }))} isBR={isBR} shuffleText={i18n('Shuffle')} isRandom />
-      </div>
-    </div>
-  </div>
+  );
 }
