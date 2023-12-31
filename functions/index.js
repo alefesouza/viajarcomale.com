0a0bb9fb23fb11addcd9297e038b05c02d74bdc3
Alefe Souza <contact@alefesouza.com>
2023-12-31 02:12:06 -0300

    Update location change function and create function for hashtag changes

--- functions/index.js
+++ functions/index.js
@@ -76,6 +76,8 @@ exports.onMediaUpdated = onDocumentUpdated(
           name_pt: data.name_pt || null,
           alternative_names: data.alternative_names || [],
           slug: data.slug,
+          latitude: data.latitude,
+          longitude: data.longitude,
         });
 
         if (!oldValue.locations || !oldValue.locations.includes(data.slug)) {
@@ -128,7 +130,7 @@ exports.onLocationUpdated = onDocumentUpdated(
     const db = getFirestore();
     const batch = db.batch();
 
-    if (oldValue.slug !== newValue.slug) {
+    if (oldValue.slug !== newValue.slug || oldValue.city !== newValue.city) {
       batch.set(
         db.doc(
           `/countries/${newValue.country}/cities/${newValue.city}/locations/${newValue.slug}`
@@ -141,12 +143,28 @@ exports.onLocationUpdated = onDocumentUpdated(
       oldValue.name !== newValue.name ||
       oldValue.name_pt !== newValue.name_pt ||
       oldValue.slug !== newValue.slug ||
+      oldValue.city !== newValue.city ||
       JSON.stringify(oldValue.alternative_names) !==
         JSON.stringify(newValue.alternative_names)
     ) {
+      let countryData = null;
+      let cityData = null;
+
+      if (oldValue.city !== newValue.city) {
+        const countryRef = await db
+          .collection('countries')
+          .doc(newValue.country)
+          .get();
+
+        countryData = countryRef.data();
+        cityData = countryData.cities.find((c) => c.slug === newValue.city);
+      }
+
       const mediasSnapshot = await db
         .collection('countries')
-        .doc(newValue.country)
+        .doc(newValue.changing_country || oldValue.country)
+        .collection('cities')
+        .doc(oldValue.city)
         .collection('medias')
         .where('locations', 'array-contains', oldValue.slug)
         .get();
@@ -178,18 +196,97 @@ exports.onLocationUpdated = onDocumentUpdated(
         }
 
         const mediaUpdate = {
+          city: newValue.city,
+          country: newValue.country,
+          ...(countryData
+            ? {
+                cityData: {
+                  name: cityData.name,
+                  name_pt: cityData.name_pt ? cityData.name_pt : null,
+                  slug: cityData.slug,
+                  end: cityData.end,
+                },
+                countryData: {
+                  name: countryData.name,
+                  name_pt: countryData.name_pt ? countryData.name_pt : null,
+                  slug: countryData.slug,
+                  iso: countryData.iso,
+                },
+              }
+            : null),
           locations,
           location_data: locationData.map((l) => ({
             name: l.name,
             name_pt: l.name_pt || null,
             slug: l.slug,
             alternative_names: l.alternative_names || [],
+            latitude: l.latitude || null,
+            longitude: l.longitude || null,
           })),
         };
 
         if (oldValue.slug !== newValue.slug) {
           mediaUpdate.location_slug_update = true;
         }
+
+        batch.update(doc.ref, mediaUpdate);
+      });
+    }
+
+    return batch.commit();
+  }
+);
+
+exports.onHashtagUpdated = onDocumentUpdated(
+  '/hashtags/{hashtagId}',
+  async (event) => {
+    const oldValue = event.data.before.data();
+    const newValue = event.data.after.data();
+
+    const db = getFirestore();
+    const batch = db.batch();
+
+    if (oldValue.name !== newValue.name) {
+      batch.set(db.doc(`/hashtags/${newValue.name}`), newValue);
+    }
+    if (
+      oldValue.name !== newValue.name ||
+      oldValue.name_pt !== newValue.name_pt
+    ) {
+      const mediasSnapshot = await db
+        .collectionGroup('medias')
+        .where('hashtags', 'array-contains', oldValue.name)
+        .orderBy('order', 'desc')
+        .get();
+
+      mediasSnapshot.forEach((doc) => {
+        let hashtags = [...new Set(doc.data().hashtags)];
+        let hashtags_pt = [...new Set(doc.data().hashtags_pt)];
+
+        const hashtagIndex = hashtags.findIndex((l) => l === oldValue.name);
+
+        hashtags[hashtagIndex] = newValue.name;
+
+        const hashtagPtIndex = hashtags_pt.findIndex(
+          (l) => l === oldValue.name
+        );
+
+        const hashtagPtPtIndex = hashtags_pt.findIndex(
+          (l) => l === oldValue.name_pt
+        );
+
+        if (hashtags_pt[hashtagPtIndex]) {
+          hashtags_pt[hashtagPtIndex] = newValue.name_pt || newValue.name;
+        }
+
+        if (hashtags_pt[hashtagPtPtIndex]) {
+          hashtags_pt[hashtagPtPtIndex] = newValue.name_pt;
+        }
+
+        const mediaUpdate = {
+          hashtags,
+          hashtags_pt,
+        };
 
         batch.update(doc.ref, mediaUpdate);
       });
